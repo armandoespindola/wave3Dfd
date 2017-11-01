61912a9650be41ec3dff9e715797ba87358e9a35
Armando Espindola-Carmona <armando.espindolacarmona@kaust.edu.sa>
2017-11-01 12:11:02 +0300

    Definitions.hpp and gitignore

---
+++ definitions.hpp
@@ -0,0 +1,28 @@
+#ifndef __definitions__
+#define __definitions__
+#include "definitions.hpp"
+
+
+// PRECISION OF OPERATIONS //
+
+#define ZERO 0.0
+#define ONE  1.0
+#define REAL double
+typedef REAL Dfloat;
+
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <new>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <omp.h>
+#include <mpi.h>
+
+
+
+
+
+#endif
