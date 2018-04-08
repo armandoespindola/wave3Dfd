1cdd29ae8bb3d3d6b7b98af24ca5e18f61b0c4c8
Armando Espindola-Carmona <armando.espindolacarmona@kaust.edu.sa>
2018-04-08 13:24:42 +0300

    GNU-License

--- src/wave3Dfd.cpp
+++ src/wave3Dfd.cpp
@@ -1,3 +1,25 @@
+/*
+                  ###  Wave3Dfd (Frequency-Kernels) 1.0 ####
+
+    Copyright (C) April 2018  Armando Espindola Carmona,
+    Universidad Nacional Autonoma de Mexico (UNAM)
+    King Abdullah University of Science and Technology (KAUST).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
 #include "definitions.hpp"
 #include "geometry3D.hpp"
 #include "model.hpp"
@@ -8,16 +30,6 @@
 #include "kernelsw.hpp"
 #include "kernels.hpp"
 #include "fdt.hpp"
-
-// ################################
-// ################################
-// ################################
-// FREQUENCY FULL WAVE FORM INVERSION
-// BY ARMANDO ESPINDOLA CARMONA
-// KAUST MARCH 2018
-// ################################
-// ################################
-// ################################
 
 
 int main (int argc, char* argv[]) {
